00d70dd381d9b0fd8582b5e7e912799dad9313d6
Irvin Homem <irvinhomem@gmail.com>
2017-02-24 14:38:03 +0100

    Added Contact class. (Defining 'data model')

---
+++ app/src/main/java/com/zwerks/androrecyclertest/Contact.java
@@ -0,0 +1,42 @@
+package com.zwerks.androrecyclertest;
+
+import java.util.ArrayList;
+
+/**
+ * Created by irvin on 24/02/2017.
+ */
+
+/**
+ * Defining a Model:
+    Every RecyclerView is backed by a source for data. In this case, we will define a Contact class
+    which represents the data model being displayed by the RecyclerView:
+ */
+public class Contact {
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online){
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName(){
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts){
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for(int i = 1; i <= numContacts; i++){
+            contacts.add(new Contact("Person " + ++lastContactId, i<= numContacts/2));
+        }
+
+        return contacts;
+    }
+}
