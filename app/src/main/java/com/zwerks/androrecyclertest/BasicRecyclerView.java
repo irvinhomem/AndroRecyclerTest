5d945100b5fcf2afdf3c0be34db64c986c849ae5
Irvin Homem <irvinhomem@gmail.com>
2017-02-24 15:22:49 +0100

    Added RecyclerViewAdapter

---
+++ app/src/main/java/com/zwerks/androrecyclertest/BasicRecyclerView.java
@@ -0,0 +1,13 @@
+package com.zwerks.androrecyclertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasicRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_recycler_view);
+    }
+}
